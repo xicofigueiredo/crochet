131b56c33fc30e80d89198cab848b4b344ce8a13
Francisco Figueiredo <francisco-abf@hotmail.com>
2023-04-18 18:06:42 +0100

    inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  def switch_locale(&action)
+    locale = session[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
